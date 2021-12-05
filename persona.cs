00d5241277a31eb21ed090f91c8a0f27d6c94a42
miraqueriko <95174155+miraqueriko@users.noreply.github.com>
2021-12-05 18:24:28 -0400

    Update persona.cs
    hice algunos cambios para reducir la cantidad de lineas de codigo y eficientizar

--- persona.cs
+++ persona.cs
@@ -1,3 +1,4 @@
+
 using System;
 
 class persona //clase base
@@ -5,153 +6,103 @@ class persona //clase base
     protected string nombre;
     protected int edad;
     protected string cedula;
-    protected string direccion;
-    protected double sueldoBase;
-  
-    public void setNombre()
+    protected int horasTrabajadas;
+    protected double pagoSemana;  
+    public void setter()
     {
-        Console.Write("\n\nIngrese su nombre: ");
+        Console.Write("\nIngrese su nombre: ");
         nombre = Console.ReadLine();
-    }
-
-    public void getNombre()
-    {
-        Console.WriteLine("\n\nSu nombre es: {0}", nombre);       
-    }
-
-    public void setEdad()
-    {
-        Console.Write("\nIngrese su Edad: ");
+        Console.Write("Ingrese su Edad: ");
         edad = int.Parse(Console.ReadLine());
-    }
-
-    public void getEdad()
-    {
-        Console.WriteLine("\nSu edad es: {0}", edad);       
-    }
-
-    public void setCedula()
-    {
-        Console.Write("\nIngrese su Cedula: ");
+        Console.Write("Ingrese su Cedula: ");
         cedula = Console.ReadLine();
+        Console.Write("Ingrese la cantidad de horas trabajadas: ");
+        horasTrabajadas = int.Parse(Console.ReadLine());
     }
-
-    public void getCedula()
+    public void getter()
     {
-        Console.WriteLine("\nSu Cedula es: {0}", cedula);       
+        Console.WriteLine("\nSu nombre es: {0}", nombre);    
+        Console.WriteLine("Su edad es: {0}", edad);  
+        Console.WriteLine("Su Cedula es: {0}", cedula); 
+        Console.WriteLine("Su cantidad de horas trabajadas es: {0}", horasTrabajadas);
     }
-
-    public void setDireccion()
+    public virtual void pagoPorSemana()
     {
-        Console.Write("\nIngrese su Direccion: ");
-        direccion = Console.ReadLine();
-    }
+        
+        if (horasTrabajadas <= 44)
+        {
+            pagoSemana = horasTrabajadas * 50.11;
+        }
+        else
+        {
+            pagoSemana = 44 * 50.11 + (horasTrabajadas-44) * 50.11 * 1.5;
+        }
 
-    public void getDireccion()
-    {
-        Console.WriteLine("\nSu Direccion es: {0}", direccion);       
+        Console.WriteLine("Su pago semanal corresponde a: {0}", pagoSemana);
     }
-
-    public virtual void Licencia() //metodo virtual para poder sobreescribir en una clase heredera
-    {
-        Console.WriteLine("\nLicencia por paternidad otorgada: 2 Dias");
-    }
-
-    public void setSueldoBase()
-    {
-        Console.Write("\nIngrese el sueldo base del puesto: ");
-        sueldoBase = double.Parse(Console.ReadLine());
-    }
-    public void getSueldoBase()
-    {
-        Console.WriteLine("\nSu sueldo base es: {0}", sueldoBase);
-    }
-
 }
 
 class hombre : persona //clase derivada
 {
-    private int esperanzaDeVida; //atributo propio de la clase
-    private int superavit; //atributo propio de la clase
-
-    public void mensaje() //metodo propio de la clase
+    protected double bonoPorPaternidad;
+    public void LicenciaPorPaternidad()
     {
-        Console.Write("\n****Este objeto es para una persona masculina****");
+        Console.WriteLine("Se le han otorgado 3 dias calendario por el nacimiento de su hijo");
     }
-
-    public void EsperanzaDeVida() //metodo propio de la clase
+    public void incentivoPadre()
     {
-        esperanzaDeVida = 74 - edad;
-
-        if (esperanzaDeVida > 0)
-        {
-            Console.Write("\nSu esperanza de vida es de: {0} años", esperanzaDeVida);
-        }
-
-        else
-        {
-            superavit = esperanzaDeVida * (-1);
-            Console.Write("\nUsted a superado la esperanza de vida por: {0} años", superavit);
-        }
+        bonoPorPaternidad = pagoSemana/2;
+        Console.WriteLine("Su bono por paternidad es: {0}", bonoPorPaternidad);
     }
-    
-
 }
 
 class mujer : persona //clase derivada
 {
-    private double sueldo; //atributo propio de la clase
-
-    public override void Licencia() //metodo sobreescrito de la clase base
+    public void LicenciaPorMaternidad()
+        {
+            Console.WriteLine("Se le han otorgado 45 dias calendario por el nacimiento de su hijo");
+        }
+    public void cuidoInfantil()
     {
-        Console.WriteLine("\nLicencia por Maternidad otorgada: 13 Semanas");
+        Console.WriteLine("A travez de RRHH se le entregara un formulario para la inscripcion de los niños menores de 3 años a la estancia infantil");
     }
-
-    public void Sueldo() //metodo propio de la clase
+    public override void pagoPorSemana() //METODO SOBREESCRITO*** 
     {
-        sueldo = sueldoBase - (sueldoBase * 0.2);
-        Console.WriteLine("\nEl sueldo promedio en base a su sueldo base es {0}", sueldo);
-    }
+        
+        if (horasTrabajadas <= 44)
+        {
+            pagoSemana = horasTrabajadas * 48.3; //CAMBIE EL VALOR POR EL QUE SE MULTIPLICAN LAS HORAS TRABAJADAS***
+        }
+        else
+        {
+            pagoSemana = 44 * 48.3 + (horasTrabajadas-44) * 48.3 * 1.5;
+        }
 
-    public void mensaje() //metodo propio de la clase
-    {
-        Console.Write("\n****Este objeto es para una persona femenina****");
+        Console.WriteLine("Su pago semanal corresponde a: {0}", pagoSemana);
     }
+    
 }
 
 class prueba //tester class
 {
-    static int Main()
+    static void Main()
     {
         Console.Clear();
-        hombre hombre1= new hombre(); //constructor clase hombre
-        mujer mujer1= new mujer(); //contructor clase mujer
-        hombre1.mensaje(); //metodo de la clase hombre
-        hombre1.setNombre(); //metodo heredado de la clase persona 
-        hombre1.setEdad(); //metodo heredado de la clase persona
-        hombre1.setCedula(); //metodo heredado de la clase persona
-        hombre1.setDireccion(); //metodo heredado de la clase persona
-        mujer1.mensaje(); //metodo de la clase mujer
-        mujer1.setNombre(); //metodo heredado de la clase persona 
-        mujer1.setEdad(); //metodo heredado de la clase persona
-        mujer1.setCedula(); //metodo heredado de la clase persona
-        mujer1.setDireccion(); //metodo heredado de la clase persona
-        mujer1.setSueldoBase(); //metodo heredado de la clase persona
-        Console.Write("\n\n*******DESPLIEGUE DE INFORMACION*******");
-        hombre1.getNombre(); //metodo heredado de la clase persona 
-        hombre1.getEdad(); //metodo heredado de la clase persona
-        hombre1.getCedula(); //metodo heredado de la clase persona
-        hombre1.getDireccion(); //metodo heredado de la clase persona
-        hombre1.Licencia(); //metodo heredado de la clase persona
-        hombre1.EsperanzaDeVida(); ////metodo de la clase hombre
-        Console.Write("\n\n***************************************");
-        mujer1.getNombre(); //metodo heredado de la clase persona 
-        mujer1.getEdad(); //metodo heredado de la clase persona
-        mujer1.getCedula(); //metodo heredado de la clase persona
-        mujer1.getDireccion(); //metodo heredado de la clase persona
-        mujer1.Licencia(); //metodo sobreescrito
-        mujer1.getSueldoBase(); //metodo heredado de la clase persona
-        mujer1.Sueldo(); //metodo de la clase mujer
-        return 0;
+        mujer mujer1 = new mujer();
+        hombre hombre1 = new hombre();
+        Console.WriteLine("****Esta entrada pertenece a una mujer****");
+        mujer1.setter();
+        Console.WriteLine("\n****Esta entrada pertenece a un hombre****");
+        hombre1.setter();
+        Console.WriteLine("*\n***RESUMEN EMPLEADA MUJER****");
+        mujer1.getter();
+        mujer1.pagoPorSemana();
+        mujer1.cuidoInfantil();
+        mujer1.LicenciaPorMaternidad();
+        Console.WriteLine("\n****RESUMEN EMPLEADO HOMBRE****");
+        hombre1.getter();
+        hombre1.pagoPorSemana();
+        hombre1.incentivoPadre();
+        hombre1.LicenciaPorPaternidad();
     }
 }
